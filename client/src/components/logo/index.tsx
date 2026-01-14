27fb1cddbd89c6a16eff40c523b4429b05c3b596
younus-always <younusofficial443@gmail.com>
2026-01-14 20:06:47 +0600

    sign up page design form and using zod validation

---
+++ client/src/components/logo/index.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+import { Link } from "react-router";
+// import LogoSvg from "@/assets/logo.svg";
+
+interface LogoProps {
+      url?: string;
+      showText?: boolean;
+      imgClass?: string;
+      textClass?: string;
+};
+
+const Logo = ({
+      url = "/",
+      showText = true,
+      imgClass = "size=[30px]",
+      textClass
+}: LogoProps) => {
+
+      return (
+            <Link to={url} className="w-fit flex items-center gap-2">
+                  <img src={""} alt="FlashChat" className={cn(imgClass)} />
+                  {showText && (
+                        <span className={cn("font-semibold text-lg leading-tight", textClass)}>FlashChat</span>
+                  )}
+            </Link>
+      )
+}
+
+export default Logo
--- client/src/pages/auth/sign-up.tsx
+++ client/src/pages/auth/sign-up.tsx
@@ -1,7 +1,128 @@
+import { useAuth } from "@/hooks/use-auth"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Logo from "@/components/logo";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { Link } from "react-router";
 
 const SignUp = () => {
+  const { register, isSigningUp } = useAuth();
+
+  const formSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email().min(1, "Email is required"),
+    password: z.string().min(6, "Password must be at least 6 characters")
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: ""
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    if (isSigningUp) return;
+    register(values);
+  };
+
   return (
-    <div>SignUp</div>
+    <div className="min-h-svh flex items-center justify-center bg-muted p-6">
+      <div className="w-full max-w-sm">
+        <Card>
+          <CardHeader
+            className="flex flex-col items-center justify-center gap-3"
+          >
+            <Logo />
+            <CardTitle className="text-xl">Create your account</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}
+                className="grid gap-4">
+                {/* name field */}
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="text"
+                          placeholder="John Doe"
+                        />
+                        <FormMessage />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                >
+                </FormField>
+
+                {/* email field */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="email"
+                          placeholder="Johndoe@example.com"
+                        />
+                        <FormMessage />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                >
+                </FormField>
+
+                {/* password field */}
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="password"
+                          placeholder="******"
+                        />
+                        <FormMessage />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                >
+                </FormField>
+
+                <Button type="submit" disabled={isSigningUp} className="w-full">
+                  {isSigningUp && <Spinner />} Sign Up
+                </Button>
+
+                <div className="text-center text-sm">
+                  Already have an account?{" "}
+                  <Link to={"/login"} className="underline">
+                    Sign In
+                  </Link>
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   )
 }
 
